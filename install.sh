0ecce0e26f85c84499d3da8703f1df79061fc4f5
Okinea Dev <81070564+okineadev@users.noreply.github.com>
2024-04-15 23:07:16 +0300

    Improved TUI installer design

--- install.sh
+++ install.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/env bash
 
+write () {
+    echo -e "\e[1;32m↪\e[0m $1"
+}
+
+echo ""
+
+write "Preparing environment"
 TEMP_DIR="$PREFIX/tmp/dotload-app"
-CURRENT_DIR=$(pwd)
 
 mkdir "$TEMP_DIR"
 cd "$TEMP_DIR"
 
-curl -sLO https://github.com/okineadev/dotload/releases/latest/download/dotload
+write "Downloading executable"
+curl -LO --progress-bar https://github.com/okineadev/dotload/releases/latest/download/dotload
 chmod +x dotload
 
+write "Installing"
 if echo "$OSTYPE" | grep -qE '^linux-android.*'; then
     cp dotload "$PREFIX/bin"
 else
     sudo ln dotload "$PREFIX/bin/dotload"
 fi
 
-cd "$CURRENT_DIR"
+write "Cleaning"
 rm -rf "$TEMP_DIR"
 
-echo "Done!"
+echo -e "\n\e[1;32mDone!\e[0m"
