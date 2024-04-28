a16277564a0a5ef786f0e04b2c7b63bb12a9f260
Okinea Dev <81070564+okineadev@users.noreply.github.com>
2024-04-28 08:52:08 +0000

    Installer updated. Added function to install dependencies

--- install.sh
+++ install.sh
@@ -1,19 +1,46 @@
 #!/usr/bin/env bash
 
+# Folder for the installation environment
 TEMP_DIR="$PREFIX/tmp"
 LOG_FILE="$TEMP_DIR/dotload-installer.log"
 
 if [[ ! -f $LOG_FILE ]]; then
+    # Create log file in temporary dir
     touch "$LOG_FILE"
 else
     # Clear all logs
     echo "" > "$LOG_FILE"
 fi
 
+sudo="sudo"
+pkgmgr=""
+update="update"
+install="install -y"
+
+if echo "$OSTYPE" | grep -qE '^linux-gnu.*'; then
+    if [ -f '/etc/debian_version' ]; then
+        pkgmgr="apt"
+    elif [ -f '/etc/arch-release' ]; then
+        pkgmgr="pacman"
+        install="-Sy --noconfirm"
+        update="-Sy"
+    fi
+
+elif echo "$OSTYPE" | grep -qE '^darwin.*'; then
+    pkgmgr="brew"
+    install="install"
+
+elif echo "$OSTYPE" | grep -qE '^linux-android.*'; then
+    pkgmgr="pkg"
+    sudo=""
+fi
+
+# Remove color markings from text (necessary to make logs cleaner)
 normalize() {
     echo -e "$1" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g"
 }
 
+# Write the text in the log
 write_log() {
     echo -e "\e[1;32m↪\e[0m $1"
     echo -e "$(normalize "$1")" >> "$LOG_FILE"
@@ -21,10 +48,11 @@ write_log() {
 
 log() {
     local command="$@"
+    # Executes the command, after which writes its output to the log
     $command 2>&1 | tee -a "$LOG_FILE"
 }
 
-step () {
+step() {
     write_log "\e[30m[$1]\e[0m \e[1m$2\e[0m"
 }
 
@@ -34,14 +62,28 @@ echo ""
 
 step "1/3" "Downloading executable"
 log curl -LO --progress-bar https://github.com/okineadev/dotload/releases/latest/download/dotload
+# Makes the script executable
 log chmod +x dotload
 
 step "2/3" "Installing"
-if echo "$OSTYPE" | grep -qE '^linux-android.*'; then
-    log cp dotload "$PREFIX/bin"
-else
-    log sudo cp dotload "$PREFIX/bin/dotload"
+if ! command -v git >/dev/null; then
+    step "2.1/3" "Installing git"
+
+    if ! -n "$pkgmgr"; then
+        echo "Please install git manually."
+        exit 1
+    else
+        if [[ "$pkgmgr" == "brew" ]]; then
+            if ! command -v brew >/dev/null; then
+                /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+            fi
+        fi
+
+        $sudo $pkgmgr $install git
+    fi
 fi
+
+log $sudo cp dotload "$PREFIX/bin"
 
 step "3/3" "Cleaning"
 log rm "$TEMP_DIR/dotload"
