b652fb3ff8b1bc122e9d3a5705520caaf60b5871
Okinea Dev <81070564+okineadev@users.noreply.github.com>
2024-06-23 18:47:51 +0300

    Create package.sh

---
+++ package.sh
@@ -0,0 +1,8 @@
+# shellcheck disable=SC2148
+# shellcheck disable=SC2034
+
+# Package metadata for instoll (https://github.com/instoll.sh/instoll)
+
+PACKAGE_NAME="dotload"
+DESCRIPTION="Simple, intuitive and fast ⚡tool for installing dotfiles and other tools on any machine!"
+TAGS="linux, dotfiles, automation, tools, packages, configuration"
